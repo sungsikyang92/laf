7d0bdbd20e9ef81fc50135880ec9e6b5a95467aa
dgl1231 <dgl12312@naver.com>
2022-07-11 14:48:31 +0900

    init

---
+++ src/test/java/com/rocket/laf/LafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.laf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
