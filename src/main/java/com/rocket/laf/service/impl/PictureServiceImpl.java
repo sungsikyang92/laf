d51ca63e4467bcc7dbdd61f36f1c3cd098776292
dgl1231 <dgl12312@naver.com>
2022-07-15 17:41:05 +0900

    Feat:makelostwrite

--- src/main/java/com/rocket/laf/service/impl/PictureServiceImpl.java
+++ src/main/java/com/rocket/laf/service/impl/PictureServiceImpl.java
@@ -9,6 +9,17 @@ import com.rocket.laf.dto.PictureDto;
 import com.rocket.laf.mapper.PictureMapper;
 import com.rocket.laf.service.PictureService;
 
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
 @Service
 public class PictureServiceImpl implements PictureService {
 
@@ -25,6 +36,65 @@ public class PictureServiceImpl implements PictureService {
     public String getMainPicLoc(long picNo) {
         // TODO Auto-generated method stub
         return pictureMapper.getMainPicLoc(picNo);
+    }
+
+    // filesystem
+
+    private String uploadPath;
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(Paths.get(uploadPath));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create upload folder!");
+        }
+
+    }
+
+    @Override
+    public void store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new Exception("ERROR : File is empty.");
+            }
+            Path root = Paths.get(uploadPath);
+            if (!Files.exists(root)) {
+                init();
+            }
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, root.resolve(file.getOriginalFilename()),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Path load(String filename) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+        // TODO Auto-generated method stub
+
     }
 
 }
