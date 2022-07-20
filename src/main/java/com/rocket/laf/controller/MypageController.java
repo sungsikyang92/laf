34b4de98822b3ec724b005db023f654110e98fea
Seungwoo.Kim <97572823+KHANUY@users.noreply.github.com>
2022-07-20 11:11:55 +0900

    Feat:ver

--- src/main/java/com/rocket/laf/controller/MypageController.java
+++ src/main/java/com/rocket/laf/controller/MypageController.java
@@ -1,6 +1,9 @@
 package com.rocket.laf.controller;
 
-@RequestMapping("/")
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MypageController {
     
 }
