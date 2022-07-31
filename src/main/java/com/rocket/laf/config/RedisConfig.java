772dec3b720bf5a3d971a74db61a66ca73968c63
sungsikyang92 <sungsik.yang92@gmail.com>
2022-07-31 21:13:47 +0900

    Feat: 1:1 Chat step1

---
+++ src/main/java/com/rocket/laf/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.rocket.laf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisConfig {
+
+    //redis pub/sub 메세지를 처리하는 listener 설정
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        return container;
+    }
+
+    //어플리케이션에서 사용할 redisTemplate 설정
+
+}
