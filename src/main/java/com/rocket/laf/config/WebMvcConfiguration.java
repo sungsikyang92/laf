62a363422e0933073cb2cb46b93e5dd88f710bbd
sungsikyang92 <sungsik.yang92@gmail.com>
2022-08-10 22:59:51 +0900

    Feat: Cors

--- src/main/java/com/rocket/laf/config/WebMvcConfiguration.java
+++ src/main/java/com/rocket/laf/config/WebMvcConfiguration.java
@@ -3,9 +3,11 @@ package com.rocket.laf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfiguration {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Bean
     public CommonsMultipartResolver commonsMultipartResolver() {
@@ -13,5 +15,14 @@ public class WebMvcConfiguration {
         commonsMultipartResolver.setDefaultEncoding("UTF-8"); //파일 인코딩 설정
         commonsMultipartResolver.setMaxUploadSizePerFile(5 * 1025 * 1024); //파일당 업로드 크기 제한(5MB)
         return commonsMultipartResolver;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
